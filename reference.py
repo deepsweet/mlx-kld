a024627780fc64092865256a7a4f7e51d1c81958
deepsweet <kir@belevi.ch>
2026-04-26 12:54:10 +0300

    🛠️ refactor reference script

--- reference.py
+++ reference.py
@@ -16,13 +16,19 @@ OUTPUT_LOG_PROBS_FILE = "reference.npy"
 OUTPUT_PROMPT_FILE = "prompt.npy"
 
 def load_full_prompt(file_path, tokenizer, max_tokens):
-    with open(file_path, 'r', encoding='utf-8') as f:
+    with open(file_path, "r", encoding="utf-8") as f:
         text = f.read()
+
     token_ids = tokenizer.encode(text)
-    if len(token_ids) < max_tokens:
-        raise ValueError(f"File too short: {len(token_ids)} < {max_tokens}")
-    if len(token_ids) > max_tokens:
+    token_len = len(token_ids)
+
+    if token_len < max_tokens:
+        raise ValueError(f"File too short: {token_len} < {max_tokens}")
+        
+    if token_len > max_tokens:
+        print(f"Truncating prompt from {token_len} to {max_tokens} tokens...")
         token_ids = token_ids[:max_tokens]
+
     return mlx.core.array(token_ids)[None]
 
 mlx.core.clear_cache()
@@ -38,5 +44,12 @@ ref_logits = ref_model(fixed_input)
 ref_log_probs = mlx.nn.log_softmax(ref_logits, axis=-1)
 
 print("Saving artifacts...")
-numpy.save(OUTPUT_LOG_PROBS_FILE, numpy.array(ref_log_probs))
-numpy.save(OUTPUT_PROMPT_FILE, numpy.array(fixed_input))
+if ref_log_probs.dtype == mlx.core.bfloat16:
+    ref_log_probs_arr = numpy.array(ref_log_probs.astype(mlx.core.float32))
+else:
+    ref_log_probs_arr = numpy.array(ref_log_probs)
+
+fixed_input_arr = numpy.array(fixed_input)
+
+numpy.save(OUTPUT_LOG_PROBS_FILE, ref_log_probs_arr)
+numpy.save(OUTPUT_PROMPT_FILE, fixed_input_arr)
